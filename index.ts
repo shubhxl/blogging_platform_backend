73e5a89eac601b008528f43af3b80456391f9e28
shubhxl <shubhamsinha1997@gmail.com>
2023-11-07 00:48:22 +0530

    routes updated

--- index.ts
+++ index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import session from 'express-session';
 import authenticationRouter from './src/routes/authentication';
+import blogRouter from './src/routes/blog';
+// import userRouter from './src/routes/user';
+// import subscriptionRouter from './src/routes/subscription';
+// import paymentRouter from './src/routes/payment';
+import {requireAuth} from './src/middleware/authMiddleware'
 import config from 'config'
-
+ 
 const app = express();
-
-
-
+ 
+ 
+ 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(
@@ -17,20 +22,19 @@ app.use(
     cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, 
   })
 );
-
-
+ 
+ 
 const port = 5000;
-
-
-
-// Use the route files for each API type
+ 
+ 
+ 
 app.use('/v1/auth', authenticationRouter);
-// app.use('/api/users', userRoutes);
-// app.use('/api/blogs', blogRoutes);
-// app.use('/api/subscriptions', subscriptionRoutes);
-// app.use('/api/payments', paymentRoutes);
-
-
+app.use('/v1/blogs', requireAuth, blogRouter);
+// app.use('/v1/users', requireAuth, userRouter);
+// app.use('/v1/subscriptions', requireAuth, subscriptionRouter);
+// app.use('/v1/payments', requireAuth, paymentRouter);
+ 
+ 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
